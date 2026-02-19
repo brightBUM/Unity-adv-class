72f3dffa206eceef6aa6815eb073fa313af6b822
Ram Manohar <81113789+brightBUM@users.noreply.github.com>
2026-02-19 12:52:25 +0530

    Shooting logic

--- Assets/Scenes/Scripts/RotateObject.cs
+++ Assets/Scenes/Scripts/RotateObject.cs
@@ -1,20 +1,47 @@
 using UnityEngine;
-
+using System;
+using System.Collections;
 public class RotateObject : MonoBehaviour
 {
-    [SerializeField] Vector3 axis;
-    [SerializeField] float amount;
+  
     [SerializeField] Transform target;
+    [SerializeField] GameObject prefab;
+    [SerializeField] bool spawnActive = true;
+    float timeBWSpawns =0.5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.Rotate(axis, amount);
-        transform.LookAt(target);
+        //var mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        //Vector2 direction = mouseWorldPos - transform.position;
+        //float angleinRadians = Mathf.Atan2(direction.y, direction.x);
+        //float angleinDegrees = angleinRadians * Mathf.Rad2Deg;
+
+        //transform.rotation = Quaternion.Euler(0,0,angleinDegrees);
+
+        StartCoroutine(CoroutineFunction());
+
+    }
+
+    IEnumerator CoroutineFunction()
+    {
+        while (spawnActive)
+        {
+            yield return new WaitForSeconds(timeBWSpawns);
+
+            var randomPoint = new Vector3(UnityEngine.Random.Range(-5.0f, 5.0f), 
+                                          UnityEngine.Random.Range(-5.0f, 5.0f), 
+                                          0f);
+
+            Instantiate(prefab, randomPoint, Quaternion.identity);
+
+            timeBWSpawns = UnityEngine.Random.Range(0.5f, 1.0f);
+            Debug.Log(timeBWSpawns);
+        }
     }
 }
--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,15 +1,28 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PlayerInput : MonoBehaviour
 {
     [SerializeField] InputActionReference moveInput;
     [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float rotSpeed = 2f;
+    [SerializeField] private float bulletSpeed = 5f;
+    [SerializeField] private float rayCastDistance = 10f;
+    [SerializeField] LayerMask damageLayer;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] int magSize = 7;
+    [SerializeField] float reloadTime = 2f;
+    int count;
     private CharacterController characterController;
+    Vector3 hitPoint;
+    bool inRange;
     private void OnEnable()
     {
         moveInput.action.performed += MoveInputPerformed;
+
     }
    
 
@@ -23,22 +36,73 @@ public class PlayerInput : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        count = magSize;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //movement
+        Movement();
+
+        //Target - mouse in XZ plane
+        Rotation();
+
+        //shoot
+        Shooting();
+
+    }
+
+    private void Shooting()
+    {
+        if (Input.GetMouseButtonDown(0) && count>0)
+        {
+            var bulletItem = Instantiate(bulletPrefab,
+                                         transform.position + transform.forward,
+                                         Quaternion.identity);
+            bulletItem.GetComponent<Rigidbody>().linearVelocity = transform.forward * bulletSpeed;
+            count--;
+            if(count<=0)
+            {
+                //invoke reload
+                StartCoroutine(Reload());
+            }
+        }
+        inRange = Physics.Raycast(transform.position, transform.forward, rayCastDistance, damageLayer);
+    }
+    IEnumerator Reload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+
+        count = magSize;
+    }
+    private void Rotation()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        if (groundPlane.Raycast(ray, out float enter))
+        {
+            hitPoint = ray.GetPoint(enter);
+            //dummyTransform.position = hitPoint;
+
+            //rotation
+            Vector3 direction = hitPoint - transform.position;
+            direction.y = 0;
+            float angleinRadians = Mathf.Atan2(direction.x, direction.z);
+            float angleinDegrees = angleinRadians * Mathf.Rad2Deg;
+
+            transform.rotation = Quaternion.Euler(0, angleinDegrees, 0);
+        }
+    }
+
+    private void Movement()
+    {
         var xInput = Input.GetAxis("Horizontal");
         var zInput = Input.GetAxis("Vertical");
-        Vector2 input = moveInput.action.ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x, 0, input.y);
 
-        //transform.position += move * Time.deltaTime * moveSpeed;
+        Vector3 move = new Vector3(xInput, 0, zInput);
+
         characterController.Move(move * Time.deltaTime * moveSpeed);
-
-        if (move != Vector3.zero)
-            transform.rotation = Quaternion.LookRotation(move);
-
     }
 
     private void OnDisable()
@@ -46,4 +110,10 @@ public class PlayerInput : MonoBehaviour
         moveInput.action.performed -= MoveInputPerformed;
 
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = inRange?Color.green:Color.red;
+        Gizmos.DrawLine(transform.position, transform.forward*rayCastDistance);
+    }
+
 }
